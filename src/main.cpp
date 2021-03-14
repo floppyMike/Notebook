1f4283ed35117d973c25a4ddf59b015e1cef31e3
Michaellu <mchblch@protonmail.com>
2021-03-14 13:55:46 +0100

    Compress to one texture

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ public:
 		if (!m_win)
 			throw std::runtime_error(SDL_GetError());
 
-		m_rend.reset(SDL_CreateRenderer(m_win.get(), -1, SDL_RENDERER_ACCELERATED));
+		m_rend.reset(SDL_CreateRenderer(m_win.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE));
 		if (!m_rend)
 			throw std::runtime_error(SDL_GetError());
 
@@ -27,29 +27,53 @@ public:
 	{
 		switch (e.type)
 		{
-		case SDL_MOUSEBUTTONDOWN: m_press = true; break;
-		case SDL_MOUSEBUTTONUP: m_press = false; break;
+		case SDL_MOUSEBUTTONDOWN:
+			m_press = true;
+			ASSERT(m_dots = sdl::Texture(
+					   SDL_CreateTexture(m_rend.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 640, 480)),
+				   "Texture dot creation failed.");
+
+			SDL_SetTextureBlendMode(m_dots.get(), SDL_BLENDMODE_BLEND);
+
+			SDL_SetRenderTarget(m_rend.get(), m_dots.get());
+			SDL_SetRenderDrawColor(m_rend.get(), 0, 0, 0, 0);
+			SDL_RenderClear(m_rend.get());
+			SDL_SetRenderTarget(m_rend.get(), nullptr);
+
+			break;
+
+		case SDL_MOUSEBUTTONUP:
+			m_press = false;
+			m_prev_dots.emplace_back(std::move(m_dots));
+			break;
+
 		case SDL_MOUSEMOTION:
 			if (m_press)
-				m_dots_loc.push_back({ e.motion.x - (m_dim >> 1), e.motion.y - (m_dim >> 1) });
+			{
+				SDL_SetRenderTarget(m_rend.get(), m_dots.get());
+
+				const SDL_Rect dest = { e.motion.x - (m_dim >> 1), e.motion.y - (m_dim >> 1), m_dim, m_dim };
+				SDL_RenderCopy(m_rend.get(), m_dot.get(), nullptr, &dest);
+
+				SDL_SetRenderTarget(m_rend.get(), nullptr);
+			}
 			break;
+
 		case SDL_MOUSEWHEEL: m_dim += e.wheel.y; break;
 		}
 	}
 	void update() {}
 	void render()
 	{
-		// SDL_SetRenderDrawColor(m_rend.get(), sdl::DARK_GRAY.r, sdl::DARK_GRAY.g, sdl::DARK_GRAY.b, sdl::DARK_GRAY.a);
 		SDL_SetRenderDrawColor(m_rend.get(), sdl::WHITE.r, sdl::WHITE.g, sdl::WHITE.b, sdl::WHITE.a);
 
 		// 	Clear window
 		SDL_RenderClear(m_rend.get());
 
-		for (SDL_Point p : m_dots_loc)
-		{
-			const SDL_Rect dest = { p.x, p.y, m_dim, m_dim };
-			SDL_RenderCopy(m_rend.get(), m_dot.get(), nullptr, &dest);
-		}
+		for (const sdl::Texture &t : m_prev_dots) SDL_RenderCopy(m_rend.get(), t.get(), nullptr, nullptr);
+
+		if (m_dots)
+			SDL_RenderCopy(m_rend.get(), m_dots.get(), nullptr, nullptr);
 
 		// Render Buffer
 		SDL_RenderPresent(m_rend.get());
@@ -59,8 +83,9 @@ private:
 	sdl::Window	  m_win;
 	sdl::Renderer m_rend;
 
-	std::vector<SDL_Point> m_dots_loc;
-	bool				   m_press = false;
+	sdl::Texture			  m_dots;
+	bool					  m_press = false;
+	std::vector<sdl::Texture> m_prev_dots;
 
 	sdl::Texture m_dot;
 	int			 m_dim = 8;
@@ -70,7 +95,7 @@ auto main() -> int
 {
 	sdl::SDL s;
 	App		 a;
-	sdl::run(&a, 30);
+	sdl::run(&a, 61);
 
 	return 0;
 }
