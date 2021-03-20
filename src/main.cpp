a58896bf3686b205c6b8600a0940ed5c7edf1c40
Michaellu <mchblch@protonmail.com>
2021-03-20 13:15:39 +0100

    Structs

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,54 @@
 
 using namespace ctl;
 
+struct Line
+{
+	Line() = default;
+
+	Line(uint8_t r, std::vector<SDL_Point> &&p)
+		: radius(r)
+		, points(std::move(p))
+	{
+	}
+
+	uint8_t				   radius = 8;
+	std::vector<SDL_Point> points;
+};
+
+struct Texture
+{
+	Texture() = default;
+
+	Texture(SDL_Rect r, sdl::Texture &&t)
+		: dim(r)
+		, data(std::move(t))
+	{
+	}
+
+	SDL_Rect	 dim;
+	sdl::Texture data;
+};
+
+auto create_empty(SDL_Renderer *r, int w, int h) noexcept -> sdl::Texture
+{
+	sdl::Texture t;
+	ASSERT(t = sdl::Texture(SDL_CreateTexture(r, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, w, h)),
+		   "Texture dot creation failed.");
+
+	SDL_SetTextureBlendMode(t.get(), SDL_BLENDMODE_BLEND);
+
+	SDL_SetRenderTarget(r, t.get());
+	SDL_SetRenderDrawColor(r, 0, 0, 0, 0);
+	SDL_RenderClear(r);
+	SDL_SetRenderTarget(r, nullptr);
+
+	return t;
+}
+
+auto shrink_to_fit(Line &l, Texture &t) -> Texture {}
+
+auto generate_draw_circle(const int r) noexcept { return mth::gen_circle_filled(r); }
+
 class App
 {
 public:
@@ -19,7 +67,7 @@ public:
 		if (!m_rend)
 			throw std::runtime_error(SDL_GetError());
 
-		m_dot = mth::gen_circle_filled(m_dim >> 1);
+		m_circle_pattern = generate_draw_circle(m_current_rad);
 	}
 
 	void pre_pass() {}
@@ -28,44 +76,41 @@ public:
 		switch (e.type)
 		{
 		case SDL_MOUSEBUTTONDOWN:
-			if (e.button.button != SDL_BUTTON_LEFT)
-				break;
-
-			m_press = true;
-			ASSERT(m_dots = sdl::Texture(
-					   SDL_CreateTexture(m_rend.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 640, 480)),
-				   "Texture dot creation failed.");
-
-			SDL_SetTextureBlendMode(m_dots.get(), SDL_BLENDMODE_BLEND);
-
-			SDL_SetRenderTarget(m_rend.get(), m_dots.get());
-			SDL_SetRenderDrawColor(m_rend.get(), 0, 0, 0, 0);
-			SDL_RenderClear(m_rend.get());
-			SDL_SetRenderTarget(m_rend.get(), nullptr);
+			if (e.button.button == SDL_BUTTON_LEFT)
+			{
+				m_press_left	 = true;
+				m_target_texture = create_empty(m_rend.get(), 640, 480);
+				// m_idx = 0;
+			}
+			else if (e.button.button == SDL_BUTTON_RIGHT)
+			{
+				m_press_right = true;
+			}
 
 			break;
 
 		case SDL_MOUSEBUTTONUP:
-			m_press = false;
-			m_prev_dots.emplace_back(std::move(m_dots));
+			m_press_left = false;
+			m_textures.emplace_back(SDL_Rect{ 0, 0, 640, 480 }, std::move(m_target_texture));
+			// m_points_idx.emplace_back(m_idx);
 			break;
 
 		case SDL_MOUSEMOTION:
-			if (m_press)
+			if (m_press_left)
 			{
-				SDL_SetRenderTarget(m_rend.get(), m_dots.get());
+				SDL_SetRenderTarget(m_rend.get(), m_target_texture.get());
 
 				SDL_SetRenderDrawColor(m_rend.get(), sdl::BLACK.r, sdl::BLACK.g, sdl::BLACK.b, sdl::BLACK.a);
 
 				// Draw circle
-				std::vector<SDL_Point> buf(m_dot.size());
-				std::transform(m_dot.begin(), m_dot.end(), buf.begin(), [&e](mth::Point<int> p) {
+				std::vector<SDL_Point> buf(m_circle_pattern.size());
+				std::transform(m_circle_pattern.begin(), m_circle_pattern.end(), buf.begin(), [&e](mth::Point<int> p) {
 					return SDL_Point{ p.x + e.motion.x, p.y + e.motion.y };
 				});
-				SDL_RenderDrawPoints(m_rend.get(), buf.data(), m_dot.size());
+				SDL_RenderDrawPoints(m_rend.get(), buf.data(), m_circle_pattern.size());
 
 				// Draw connecting line
-				for (auto i = -(m_dim >> 1); i < (m_dim >> 1); ++i)
+				for (int i = -m_current_rad, end = m_current_rad; i < end; ++i)
 				{
 					SDL_RenderDrawLine(m_rend.get(), e.motion.x - e.motion.xrel, e.motion.y - e.motion.yrel + i,
 									   e.motion.x, e.motion.y + i);
@@ -74,14 +119,30 @@ public:
 									   e.motion.x + i, e.motion.y);
 				}
 
+				m_target_line.points.push_back({ e.motion.x - m_current_rad, e.motion.y - m_current_rad });
+				// ++m_idx;
+
 				SDL_SetRenderTarget(m_rend.get(), nullptr);
+			}
+			if (m_press_right)
+			{
+				// const auto mouse	 = SDL_Point{ e.motion.x, e.motion.y };
+				// const auto intersect = std::find_if(
+				// 	m_points.rbegin(), m_points.rend(),
+				// 	[m = SDL_Point{ e.motion.x, e.motion.y }](const SDL_Rect r) { return SDL_PointInRect(&m, &r); });
+
+				// if (intersect == m_points.rend())
+				// 	break;
+
+				// const auto idx = std::lower_bound(m_points_idx.begin(), m_points_idx.end(),
+				// 								  std::distance(intersect, m_points.rend()));
 			}
 
 			break;
 
 		case SDL_MOUSEWHEEL:
-			m_dim += e.wheel.y;
-			m_dot = mth::gen_circle_filled(m_dim >> 1);
+			m_current_rad += e.wheel.y;
+			m_circle_pattern = generate_draw_circle(m_current_rad);
 			break;
 		}
 	}
@@ -93,10 +154,10 @@ public:
 		// 	Clear window
 		SDL_RenderClear(m_rend.get());
 
-		for (const sdl::Texture &t : m_prev_dots) SDL_RenderCopy(m_rend.get(), t.get(), nullptr, nullptr);
+		for (const Texture &t : m_textures) SDL_RenderCopy(m_rend.get(), t.data.get(), nullptr, &t.dim);
 
-		if (m_dots)
-			SDL_RenderCopy(m_rend.get(), m_dots.get(), nullptr, nullptr);
+		if (!m_target_line.points.empty())
+			SDL_RenderCopy(m_rend.get(), m_target_texture.get(), nullptr, nullptr);
 
 		// Render Buffer
 		SDL_RenderPresent(m_rend.get());
@@ -106,12 +167,22 @@ private:
 	sdl::Window	  m_win;
 	sdl::Renderer m_rend;
 
-	sdl::Texture			  m_dots;
-	bool					  m_press = false;
-	std::vector<sdl::Texture> m_prev_dots;
+	sdl::Texture m_target_texture;
+	uint8_t		 m_current_rad = 1;
+	Line		 m_target_line;
 
-	int							 m_dim = 8;
-	std::vector<mth::Point<int>> m_dot;
+	bool m_press_left  = false;
+	bool m_press_right = false;
+
+	std::vector<Texture> m_textures;
+	std::vector<Line>	 m_lines;
+	size_t				 m_idx;
+
+	// size_t				  m_idx;
+	// std::vector<SDL_Rect> m_points;
+	// std::vector<size_t>	  m_points_idx = { 0 };
+
+	std::vector<mth::Point<int>> m_circle_pattern;
 };
 
 auto main() -> int
