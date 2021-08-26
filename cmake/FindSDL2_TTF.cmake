f8f6a2884bebd72adf8945d446a74ef266a2150e
Michaellu <mchblch@protonmail.com>
2021-08-26 07:39:51 +0200

    Updated ttf search

--- cmake/FindSDL2_TTF.cmake
+++ cmake/FindSDL2_TTF.cmake
@@ -5,14 +5,7 @@ FIND_PATH(SDL2_TTF_INCLUDE_DIR SDL_ttf.h
 	$ENV{SDL2_TTF}
 	PATH_SUFFIXES include/SDL2 include SDL2
 	PATHS
-	~/Library/Frameworks
-	/Library/Frameworks
-	/usr/local/include/SDL2
-	/usr/include/SDL2
-	/sw # Fink
-	/opt/local # DarwinPorts
-	/opt/csw # Blastwave
-	/opt
+	/usr
 	"extern/SDL2_ttf-2.0.15"
 )
 
@@ -24,18 +17,13 @@ FIND_LIBRARY(SDL2_TTF_LIBRARY_TEMP
 	$ENV{SDL2_TTF}
 	PATH_SUFFIXES lib64 lib lib/x64
 	PATHS
-	/sw
-	/opt/local
-	/opt/csw
-	/opt
+	/usr
 	"extern/SDL2_ttf-2.0.15"
 )
 
 SET(SDL2_TTF_FOUND "NO")
 	IF(SDL2_TTF_LIBRARY_TEMP)
-	# Set the final string here so the GUI reflects the final state.
 	SET(SDL2_TTF_LIBRARY ${SDL2_TTF_LIBRARY_TEMP} CACHE STRING "Where the SDL2_ttf Library can be found")
-	# Set the temp variable to INTERNAL so it is not seen in the CMake GUI
 	SET(SDL2_TTF_LIBRARY_TEMP "${SDL2_TTF_LIBRARY_TEMP}" CACHE INTERNAL "")
 	SET(SDL2_TTF_FOUND "YES")
 ENDIF(SDL2_TTF_LIBRARY_TEMP)
