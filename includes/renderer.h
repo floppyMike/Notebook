5e92710fd218eec2b14ce3e13c3148a7b8fa44f9
Michaellu <mchblch@protonmail.com>
2021-09-11 17:14:45 +0200

    Added renderer concept

--- includes/renderer.h
+++ includes/renderer.h
@@ -1,169 +1,39 @@
 #pragma once
 
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include "renderer/sdl2.h"
 
-#include <CustomLibrary/SDL/All.h>
+void nothing();
 
-using namespace ctl;
-
-using TextureData = sdl::Texture;
-using FontData	  = sdl::Font;
-
-struct RendererContext
+// clang-format off
+template<typename T>
+concept is_renderer = requires(T t)
 {
-	sdl::Renderer r;
-	bool		  refresh = true;
+	T((SDL_Window *)nullptr);
 
-	std::vector<mth::Point<int>> circle_pattern;
+	t.set_draw_color(SDL_Color{});
+	t.set_render_target(TextureData());
+	t.set_stroke_radius(int());
+
+	t.render_target();
+
+	t.draw_joint_stroke(mth::Point<int>(), int());
+	t.draw_con_stroke(mth::Point<int>(), mth::Point<int>(), int());
+
+	{ t.get_texture_size(TextureData()) } -> std::same_as<mth::Dim<int>>;
+	
+	{ t.create_texture(int(), int()) } -> std::same_as<TextureData>;
+	{ t.create_font("", int()) } -> std::same_as<FontData>;
+	{ t.create_text(FontData(), "") } -> std::same_as<TextureData>;
+
+	t.draw_texture(TextureData(), mth::Rect<int>());
+	t.draw_rect(mth::Rect<int>());
+	t.draw_line(mth::Point<int>(), mth::Point<int>());
+
+	{ t.crop_texture(TextureData(), mth::Rect<int>()) } -> std::same_as<TextureData>;
+	t.refresh();
+
+	t.render(nothing);
 };
+// clang-format on
 
-inline auto translate_circle(const RendererContext &c, mth::Point<int> m) -> std::vector<SDL_Point>
-{
-	std::vector<SDL_Point> buf(c.circle_pattern.size());
-	std::transform(c.circle_pattern.begin(), c.circle_pattern.end(), buf.begin(),
-				   [m](mth::Point<int> p) {
-					   return SDL_Point{ p.x + m.x, p.y + m.y };
-				   });
-
-	return buf;
-}
-
-class Renderer
-{
-public:
-	explicit Renderer(SDL_Window *win)
-	{
-		m_con.r.reset(SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE));
-
-		if (!m_con.r)
-			throw std::runtime_error(SDL_GetError());
-
-		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"); // Enable a blur effect when copying textures
-	}
-
-	void set_draw_color(SDL_Color col) const
-	{
-		SDL_SetRenderDrawColor(m_con.r.get(), col.r, col.g, col.b, col.a);
-	}
-
-	void set_render_target(const TextureData &t)
-	{
-		SDL_SetRenderTarget(m_con.r.get(), t.get());
-	}
-
-	auto get_texture_size(const TextureData &t)
-	{
-		mth::Dim<int> d;
-		SDL_QueryTexture(t.get(), nullptr, nullptr, &d.w, &d.h);
-
-		return d;
-	}
-
-	void render_target()
-	{
-		SDL_RenderPresent(m_con.r.get());
-	}
-
-	void set_stroke_radius(int r)
-	{
-		m_con.circle_pattern.clear();
-
-		for (auto x = -r; x <= +r; ++x)
-			for (auto y = -r; y <= +r; ++y)
-				if (y * y + x * x < r * r)
-					m_con.circle_pattern.push_back({ x, y });
-	}
-
-	void draw_joint_stroke(mth::Point<int> p, int r) const
-	{
-		assert(!m_con.circle_pattern.empty() && "Circle pattern not generated yet.");
-
-		const auto buf = translate_circle(m_con, p);
-		SDL_RenderDrawPoints(m_con.r.get(), buf.data(), (int)m_con.circle_pattern.size());
-	}
-
-	void draw_con_stroke(mth::Point<int> from, mth::Point<int> to, int r) const
-	{
-		for (int i = -(r - 1); i <= r - 1; ++i)
-		{
-			SDL_RenderDrawLine(m_con.r.get(), from.x, from.y + i, to.x, to.y + i);
-			SDL_RenderDrawLine(m_con.r.get(), from.x + i, from.y, to.x + i, to.y);
-		}
-	}
-
-	auto create_texture(int w, int h) const
-	{
-		return sdl::create_empty(m_con.r.get(), w, h);
-	}
-
-	auto create_font(const char *path, int size)
-	{
-		return sdl::load_font(path, size);
-	}
-
-	auto create_text(const FontData &f, const char *text) const
-	{
-		auto *s = TTF_RenderText_Blended_Wrapped(f.get(), text, sdl::BLACK, 600);
-
-		if (s == nullptr)
-			throw std::runtime_error(TTF_GetError());
-
-		auto t = TextureData(SDL_CreateTextureFromSurface(m_con.r.get(), s));
-
-		SDL_FreeSurface(s);
-
-		return t;
-	}
-
-	auto crop_texture(const TextureData &t, const mth::Rect<int> &r) const
-	{
-		return sdl::crop(m_con.r.get(), t, r);
-	}
-
-	void draw_texture(const TextureData &t, const mth::Rect<int> &r) const
-	{
-		SDL_RenderCopy(m_con.r.get(), t.get(), nullptr, &sdl::to_rect(r));
-	}
-
-	void draw_rect(mth::Rect<int> r) const
-	{
-		SDL_RenderDrawRect(m_con.r.get(), &sdl::to_rect(r));
-	}
-
-	void draw_rect(mth::Rect<float> r) const
-	{
-		SDL_RenderDrawRectF(m_con.r.get(), &sdl::to_rect(r));
-	}
-
-	void draw_line(mth::Point<int> start, mth::Point<int> end) const
-	{
-		SDL_RenderDrawLine(m_con.r.get(), start.x, start.y, end.x, end.y);
-	}
-
-	void refresh()
-	{
-		m_con.refresh = true;
-	}
-
-	template<typename T>
-	void render(T &&draws)
-	{
-		if (!m_con.refresh)
-			return;
-
-		SDL_SetRenderTarget(m_con.r.get(), nullptr);
-
-		SDL_SetRenderDrawColor(m_con.r.get(), sdl::WHITE.r, sdl::WHITE.g, sdl::WHITE.b, sdl::WHITE.a);
-		SDL_RenderClear(m_con.r.get());
-
-		draws();
-
-		SDL_RenderPresent(m_con.r.get());
-
-		m_con.refresh = false;
-	}
-
-private:
-	RendererContext m_con;
-};
+static_assert(is_renderer<Renderer>);
---
+++ includes/renderer/sdl2.h
@@ -0,0 +1,166 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+#include <CustomLibrary/SDL/All.h>
+
+using namespace ctl;
+
+using TextureData = sdl::Texture;
+using FontData	  = sdl::Font;
+
+struct RendererContext
+{
+	sdl::Renderer r;
+	bool		  refresh = true;
+
+	std::vector<mth::Point<int>> circle_pattern;
+};
+
+inline auto translate_circle(const RendererContext &c, mth::Point<int> m) -> std::vector<SDL_Point>
+{
+	std::vector<SDL_Point> buf(c.circle_pattern.size());
+	std::transform(c.circle_pattern.begin(), c.circle_pattern.end(), buf.begin(),
+				   [m](mth::Point<int> p) {
+					   return SDL_Point{ p.x + m.x, p.y + m.y };
+				   });
+
+	return buf;
+}
+
+class Renderer
+{
+public:
+	explicit Renderer(SDL_Window *win)
+	{
+		m_con.r.reset(SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE));
+
+		if (!m_con.r)
+			throw std::runtime_error(SDL_GetError());
+
+		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"); // Enable a blur effect when copying textures
+	}
+
+	void set_draw_color(SDL_Color col) const
+	{
+		SDL_SetRenderDrawColor(m_con.r.get(), col.r, col.g, col.b, col.a);
+	}
+
+	void set_render_target(const TextureData &t)
+	{
+		SDL_SetRenderTarget(m_con.r.get(), t.get());
+	}
+
+	auto get_texture_size(const TextureData &t)
+	{
+		mth::Dim<int> d;
+		SDL_QueryTexture(t.get(), nullptr, nullptr, &d.w, &d.h);
+
+		return d;
+	}
+
+	void render_target()
+	{
+		SDL_RenderPresent(m_con.r.get());
+	}
+
+	void set_stroke_radius(int r)
+	{
+		m_con.circle_pattern.clear();
+
+		for (auto x = -r; x <= +r; ++x)
+			for (auto y = -r; y <= +r; ++y)
+				if (y * y + x * x < r * r)
+					m_con.circle_pattern.push_back({ x, y });
+	}
+
+	void draw_joint_stroke(mth::Point<int> p, int r) const
+	{
+		assert(!m_con.circle_pattern.empty() && "Circle pattern not generated yet.");
+
+		const auto buf = translate_circle(m_con, p);
+		SDL_RenderDrawPoints(m_con.r.get(), buf.data(), (int)m_con.circle_pattern.size());
+	}
+
+	void draw_con_stroke(mth::Point<int> from, mth::Point<int> to, int r) const
+	{
+		for (int i = -(r - 1); i <= r - 1; ++i)
+		{
+			SDL_RenderDrawLine(m_con.r.get(), from.x, from.y + i, to.x, to.y + i);
+			SDL_RenderDrawLine(m_con.r.get(), from.x + i, from.y, to.x + i, to.y);
+		}
+	}
+
+	auto create_texture(int w, int h) const
+	{
+		return sdl::create_empty(m_con.r.get(), w, h);
+	}
+
+	auto create_font(const char *path, int size)
+	{
+		return sdl::load_font(path, size);
+	}
+
+	auto create_text(const FontData &f, const char *text) const
+	{
+		auto *s = TTF_RenderText_Blended_Wrapped(f.get(), text, sdl::BLACK, 600);
+
+		if (s == nullptr)
+			throw std::runtime_error(TTF_GetError());
+
+		auto t = TextureData(SDL_CreateTextureFromSurface(m_con.r.get(), s));
+
+		SDL_FreeSurface(s);
+
+		return t;
+	}
+
+	auto crop_texture(const TextureData &t, const mth::Rect<int> &r) const
+	{
+		return sdl::crop(m_con.r.get(), t, r);
+	}
+
+	void draw_texture(const TextureData &t, const mth::Rect<int> &r) const
+	{
+		SDL_RenderCopy(m_con.r.get(), t.get(), nullptr, &sdl::to_rect(r));
+	}
+
+	void draw_rect(mth::Rect<int> r) const
+	{
+		SDL_RenderDrawRect(m_con.r.get(), &sdl::to_rect(r));
+	}
+
+	void draw_line(mth::Point<int> start, mth::Point<int> end) const
+	{
+		SDL_RenderDrawLine(m_con.r.get(), start.x, start.y, end.x, end.y);
+	}
+
+	void refresh()
+	{
+		m_con.refresh = true;
+	}
+
+	template<typename T>
+	requires std::is_invocable_v<T>
+	void render(T &&draws)
+	{
+		if (!m_con.refresh)
+			return;
+
+		SDL_SetRenderTarget(m_con.r.get(), nullptr);
+
+		SDL_SetRenderDrawColor(m_con.r.get(), sdl::WHITE.r, sdl::WHITE.g, sdl::WHITE.b, sdl::WHITE.a);
+		SDL_RenderClear(m_con.r.get());
+
+		draws();
+
+		SDL_RenderPresent(m_con.r.get());
+
+		m_con.refresh = false;
+	}
+
+private:
+	RendererContext m_con;
+	struct RendererContext;
+};
